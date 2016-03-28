50031812575eaf478769daec9d76d8864e8a50d1
unknown <1403369@RG43025.rgu.ac.uk>
2016-03-28 10:37:25 +0100

    Initial commit

---
+++ js/Document1.js
@@ -0,0 +1,18 @@
+
+$(function(){
+
+	$("i").hover(
+		$(this).filter(':not(:animated)').animate({
+			'width':''},500);
+
+		)
+
+	function(){
+		$(this).animate({
+
+			}
+})
+
+$("i").moveover(function(){ $(this).animate({'width':'300px'},500);
+
+$("i").mouseout(function(){ $(this).animate({'width':'300px'},500);

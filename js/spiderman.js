122af725e3fb236d172dae6342623b66c63ddd9b
mackintosh95 <stuartmackintosh1995@gmail.com>
2016-04-29 00:02:36 +0100

    Update spiderman.js

--- js/spiderman.js
+++ js/spiderman.js
@@ -1,5 +1,4 @@
-
-//values
+//this will not work
 var private = '63823d9c3343ef191f88b0f44c0dbe6d7cce7e4e';
 var public = '6f769846a34acb7b0299a000d488f562';
 var id = '1009610';
@@ -8,21 +7,15 @@ var string = ts + private + public;
 var hash = CryptoJS.MD5(string);
 
 
-$(function()){
-	
-	var $header = $('#header');
-	
-	$.ajax({
-		type: 'GET',
-		url: "http://gateway.marvel.com:80/v1/public/characters/id?ts="+ts+"&hash="+hash+"apikey=" + public,
-		success: function(data){
-			//console.log(url) {
-				$.each(data, function(i, item){
-					$header.append('' + item.data.results.name + '');
-					
-				})
-				
-			//}
-		}
+
+function getMarvel(){
+	var $h1 = $('#h1');
+	var url = "http://gateway.marvel.com:80/v1/public/characters/id?ts="+ts+"&hash="+hash+"apikey="+public;
+	alert(hash);
+	alert(url);
+	console.log(url);
+	$.ajax({url: url, context:$h1}).done(function(data){
+		$('#results').html('');
+		$('results').append(data.name);
 	})
-}
+}//end of function

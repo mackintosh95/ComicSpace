eeeaf885b6dd8b84681321d6fde903b569a72795
zoeelawrence <lawrenzoee@gmail.com>
2016-04-29 14:28:17 +0100

    Update spiderman.js

--- js/spiderman.js
+++ js/spiderman.js
@@ -5,7 +5,7 @@ var id = '1009610';
 var ts = new Date().getTime();
 var crypto = require('crypto');
 var string = ts + private_key + public_key;
-var hash = crypto.createHash('md5').update(string).digest('hex'));
+var hash = crypto.createHash('md5').update(string).digest('hex');
 
 
 

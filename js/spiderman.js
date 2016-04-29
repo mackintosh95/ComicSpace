4cba4907a7dd07f4a90c8b86c0cddc67c56f9f84
mackintosh95 <stuartmackintosh1995@gmail.com>
2016-04-29 01:53:13 +0100

    Update spiderman.js

--- js/spiderman.js
+++ js/spiderman.js
@@ -1,10 +1,17 @@
 // Public Key
 // 6f769846a34acb7b0299a000d488f562
 
+var private_key = '63823d9c3343ef191f88b0f44c0dbe6d7cce7e4e';
+var public_key = '6f769846a34acb7b0299a000d488f562';
+var id = '1009610';
+var ts = new Date().getTime();
+var string = ts + private_key + public_key;
+var hash = CryptoJS.MD5(string);
+
 $(function(){
-var marvelAPI = 'https://gateway.marvel.com/v1/public/characters/1009610?';
-$.getJSON( marvelAPI, {
-    apikey: '6f769846a34acb7b0299a000d488f562'
+var marvelAPI = "http://gateway.marvel.com:80/v1/public/characters/id?ts="+ts+"&hash="+hash+"apikey="+public_key;
+//$.getJSON( marvelAPI, {
+    //apikey: '6f769846a34acb7b0299a000d488f562'
   })
     .done(function( response ) {
       	var results = response.data.results;

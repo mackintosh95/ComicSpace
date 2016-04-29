59a9116a4727ba068f38e96eba8a51936ae106ab
mackintosh95 <stuartmackintosh1995@gmail.com>
2016-04-29 02:10:58 +0100

    Update spiderman.js

--- js/spiderman.js
+++ js/spiderman.js
@@ -1,34 +1,36 @@
-// Public Key
-// 6f769846a34acb7b0299a000d488f562
 
 var private_key = '63823d9c3343ef191f88b0f44c0dbe6d7cce7e4e';
 var public_key = '6f769846a34acb7b0299a000d488f562';
 var id = '1009610';
 var ts = new Date().getTime();
-var string = ts + private_key + public_key;
+var string = ts + private + public;
 var hash = CryptoJS.MD5(string);
 
-$(function(){
-var marvelAPI = "http://gateway.marvel.com:80/v1/public/characters/id?ts="+ts+"&hash="+hash+"apikey="+public_key;
-//$.getJSON( marvelAPI, {
-    //apikey: '6f769846a34acb7b0299a000d488f562'
-  })
-    .done(function( response ) {
-      	var results = response.data.results;
-      	var resultsLen = results.length;
-      	//var output = '<ul>'; 
-      	var name = '';
-	  
-	//var name = results
-      	for(var i=0; i<resultsLen; i++){
-        	//if(results[i].images.length > 0) {
-          		name += results[i].name;
-			//output += '<li><img src="' + imgPath + '"><br>'+results[i].title+'</li>';
-        	//}
-      	}  
-      	//output += '</ul>'
-      	//$('#results').append(output);
-	$('#h1').append(name);
+
+
+$(function()){
+	
+	var $header = $('#h1');
+	
+	$.ajax({
+		type: 'GET',
+		url: "'https://gateway.marvel.com/v1/public/characters/1009610?ts="+ts+"&hash="+hash+"apikey=" + public_key,
+		success: function(data){
+			//console.log(url) {
+      var results = date.data.results;
+      var resultsLen = results.length;
+      var output = ''; 
+      
+      for(var i=0; i<resultsLen; i++){
+        //if(results[i].images.length > 0) {
+          //var imgPath = results[i].images[0].path + '/standard_xlarge.' + results[i].images[0].extension;
+          output += '' +results[i].name;
+        }
+      } 
+      $header.append(output);
   });
-   
-});
+				
+			//}
+		}
+	)
+}

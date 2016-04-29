83912354eb271c6372cee3c51e5cb8fdd91d85b2
mackintosh95 <stuartmackintosh1995@gmail.com>
2016-04-29 03:31:13 +0100

    Update spiderman.js

--- js/spiderman.js
+++ js/spiderman.js
@@ -3,7 +3,10 @@ var private_key = '63823d9c3343ef191f88b0f44c0dbe6d7cce7e4e';
 var public_key = '6f769846a34acb7b0299a000d488f562';
 var id = '1009610';
 var ts = new Date().getTime();
-var string = ts + private + public;
+//var crypto = require('crypto');
+var string = ts + private_key + public_key;
+//console.log(crypto.createHash('md5').update(string).digest());
+
 var hash = CryptoJS.MD5(string);
 
 
